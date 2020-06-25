cc33fb29f0d6fe2ed80f24e9b4ca979d4f6110f7
rubcarmo <jesus.rubens@gmail.com>
2020-06-25 12:11:00 -0300

    estado e cidade

--- src/main/java/com/rubens/PedidosApplication.java
+++ src/main/java/com/rubens/PedidosApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rubens.domain.Categoria;
+import com.rubens.domain.Cidade;
+import com.rubens.domain.Estado;
 import com.rubens.domain.Produto;
 import com.rubens.repositories.CategoriaRepository;
+import com.rubens.repositories.CidadeRepository;
+import com.rubens.repositories.EstadoRepository;
 import com.rubens.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class PedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -43,8 +53,23 @@ public class PedidosApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+				
+
 		
 	}
 
