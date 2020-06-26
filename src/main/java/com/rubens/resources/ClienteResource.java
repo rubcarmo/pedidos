e9f92f5aaf56f6f326065d9def0ebcb994c17864
rubcarmo <jesus.rubens@gmail.com>
2020-06-26 09:23:11 -0300

    Atualizando Categoria com put

--- src/main/java/com/rubens/resources/ClienteResource.java
+++ src/main/java/com/rubens/resources/ClienteResource.java
@@ -1,9 +1,5 @@
 package com.rubens.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +18,9 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}" , method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
-		Cliente obj = service.buscar(id);
+		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
