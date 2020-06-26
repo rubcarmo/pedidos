127d861efbfa5ec7854d3d0cb43f9654ee19f5a8
rubcarmo <jesus.rubens@gmail.com>
2020-06-26 09:05:00 -0300

    Inserindo uma nova Categoria com post

--- src/main/java/com/rubens/resources/CategoriaResource.java
+++ src/main/java/com/rubens/resources/CategoriaResource.java
@@ -1,15 +1,15 @@
 package com.rubens.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rubens.domain.Categoria;
 import com.rubens.services.CategoriaService;
@@ -27,6 +27,14 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/rubens/services/CategoriaService.java
+++ src/main/java/com/rubens/services/CategoriaService.java
@@ -22,4 +22,10 @@ public class CategoriaService {
 		
 	}
 
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		
+		return repo.save(obj);
+	}
+
 }
